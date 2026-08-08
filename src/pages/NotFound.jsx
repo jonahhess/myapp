4cf8ce5d77704cd881f5ce8cc28e56c2f549d577
jonahhess <jonahhessdev@gmail.com>
2026-08-09 00:06:57 +0300

    fleshed out 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <main>
-      <h1>404</h1>
-      <p>Page not found.</p>
-      <Link to="/">Go home</Link>
+    <main className="not-found-page">
+      <h1>Page Not Found</h1>
+      <p className="not-found-page__text">
+        The page you are looking for does not exist or has been moved.
+      </p>
+      <Link className="not-found-page__home-link" to="/">
+        Return to Home
+      </Link>
     </main>
   );
 }
